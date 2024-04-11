4afaab592c135c0658f6c6d31b22c81603805edc
Muhammad Haroon Ali <haroon8391@gmail.com>
2024-04-11 11:00:14 +0500

    update changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -37,7 +37,7 @@ export default function FooterCom() {
               </Footer.LinkGroup>
             </div>
             <div>
-              <Footer.Title title="Follow us" />
+              <Footer.Title title="Follow Me" />
               <Footer.LinkGroup col>
                 <Footer.Link
                   href="https://github.com/haroon8391"
@@ -46,14 +46,6 @@ export default function FooterCom() {
                 >
                   Github
                 </Footer.Link>
-                <Footer.Link href="#">Discord</Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title="Legal" />
-              <Footer.LinkGroup col>
-                <Footer.Link href="#">Privacy Policy</Footer.Link>
-                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
               </Footer.LinkGroup>
             </div>
           </div>
@@ -70,7 +62,6 @@ export default function FooterCom() {
             <Footer.Icon href="#" icon={BsInstagram} />
             <Footer.Icon href="#" icon={BsTwitter} />
             <Footer.Icon href="https://github.com/haroon8391" icon={BsGithub} />
-            <Footer.Icon href="#" icon={BsDribbble} />
           </div>
         </div>
       </div>
